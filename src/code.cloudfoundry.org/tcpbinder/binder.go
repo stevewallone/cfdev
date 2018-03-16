2d9e6e34fba9bc778a4e324a16e0e7c4e25571cd
Emily Casey <ecasey@pivotal.io>
2018-03-16 09:57:53 -0400

    WIP

--- src/code.cloudfoundry.org/tcpbinder/binder.go
+++ src/code.cloudfoundry.org/tcpbinder/binder.go
@@ -11,65 +11,123 @@ import (
 	"syscall"
 )
 
-func main() {
-	ln, err := net.ListenUnix("unix", &net.UnixAddr{
+type Binder struct {
+	socket string
+	conn   *net.UnixListener
+}
+
+func New(socket string) (*Binder, error) {
+	conn, err := net.ListenUnix("unix", &net.UnixAddr{
 		Net:  "unix",
-		Name: "/var/tmp/com.docker.vmnetd.socket",
+		Name: socket,
 	})
+	os.Chmod(socket, 0666)
+	return &Binder{
+		socket: socket,
+		conn:   conn,
+	}, err
+}
+
+func (b *Binder) Cleanup() {
+	fmt.Println("Cleanup")
+	os.Remove(b.socket)
+	b.conn.Close()
+}
+
+func (b *Binder) Socket() string {
+	return b.socket
+}
+
+func (b *Binder) Accept() (*net.UnixConn, error) {
+	return b.conn.AcceptUnix()
+}
+
+func main() {
+	binder, err := New("/var/tmp/com.docker.vmnetd.socket")
 	if err != nil {
 		panic("I can't listen!!")
 	}
-	defer os.Remove("/var/tmp/com.docker.vmnetd.socket")
-	defer ln.Close()
-	os.Chmod("/var/tmp/com.docker.vmnetd.socket", 0666)
-	fmt.Println("Listening on socket at /var/tmp/com.docker.vmnetd.socket")
+	defer binder.Cleanup()
+	fmt.Println("Listening on socket at", binder.Socket())
 
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
-	go func(ln net.Listener, c chan os.Signal) {
+	go func(c chan os.Signal, binder *Binder) {
 		sig := <-c
 		log.Printf("Caught signal %s: shutting down.", sig)
-		ln.Close()
-		os.Remove("/var/tmp/com.docker.vmnetd.socket")
+		binder.Cleanup()
 		os.Exit(0)
-	}(ln, sigc)
+	}(sigc, binder)
 
 	for {
-		conn, err := ln.AcceptUnix()
-		fmt.Println("connection received")
+		conn, err := binder.Accept()
 		if err != nil {
 			log.Fatal("Accept error: ", err)
 		}
-		sayHi(conn)
-		addr := parseReq(conn)
-		fmt.Printf("received request for %s:%d \n", addr.IP, addr.Port)
-		fmt.Println("binding")
-		file := bind(addr)
-		fmt.Printf("Opened fd: %d, name: %s \n", file.Fd(), file.Name())
-		fmt.Println("sending fd")
-		_, _, err = conn.WriteMsgUnix(make([]byte, 8, 8), syscall.UnixRights(int(file.Fd())), nil)
-		if err != nil {
-			log.Fatal("Error sending fd: ", err)
-		}
-		conn.Close()
+		go Run(conn)
 	}
 }
 
-func bind(addr *net.TCPAddr) *os.File {
+func Run(conn *net.UnixConn) {
+	defer conn.Close()
+	handshake(conn)
+	addr, err := parseReq(conn)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("received request for %s:%d \n", addr.IP, addr.Port)
+
+	file, err := bind(addr)
+	if file != nil {
+		defer file.Close()
+	}
+
+	msg, scmsg := response(file, err)
+	fmt.Printf("sending msg: %+v scmsg %+v\n", msg, scmsg)
+	if _, _, err := conn.WriteMsgUnix(msg, scmsg, nil); err != nil {
+		fmt.Println("Error writing fd msg: ", err)
+		return
+	}
+}
+
+func bind(addr *net.TCPAddr) (*os.File, error) {
 	listener, err := net.ListenTCP("tcp", addr)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	file, err := listener.File()
-	if err != nil {
-		panic(err)
-	}
-	return file
+	defer listener.Close()
+	return listener.File()
 }
 
-func sayHi(conn net.Conn) {
+func response(file *os.File, err error) ([]byte, []byte) {
+	msg := make([]byte, 8, 8)
+	var scmsg []byte
+	if err != nil {
+		if opErr, ok := err.(*net.OpError); ok {
+			if sysErr, ok := opErr.Err.(*os.SyscallError); ok {
+				switch sysErr.Err {
+				case syscall.EADDRINUSE:
+					msg[0] = uint8(48)
+				case syscall.EADDRNOTAVAIL:
+					msg[0] = uint8(49)
+				default:
+					msg[0] = uint8(66)
+				}
+			}
+		}
+		fmt.Println("Failed to bind: ", err)
+	}
+	if file != nil {
+		scmsg = syscall.UnixRights(int(file.Fd()))
+	}
+	return msg, scmsg
+}
+
+func handshake(conn net.Conn) {
 	init := make([]byte, 49, 49)
 	io.ReadFull(conn, init)
+	fmt.Printf("Connection received from: %s\n", init[0:5])
 
 	var version uint32 = 22
 	conn.Write([]byte("CFD3V"))
@@ -77,23 +135,20 @@ func sayHi(conn net.Conn) {
 	conn.Write([]byte("0123456789012345678901234567890123456789"))
 }
 
-func parseReq(conn net.Conn) *net.TCPAddr {
-	rawheader := make([]byte, 1, 1)
-	rawIP := make([]byte, 4, 4)
-	rawPort := make([]byte, 2, 2)
+func parseReq(conn net.Conn) (*net.TCPAddr, error) {
+	var instr uint8
+	ip := make([]byte, 4, 4)
+	var port uint16
+	binary.Read(conn, binary.LittleEndian, &instr)
+	binary.Read(conn, binary.LittleEndian, ip)
+	binary.Read(conn, binary.LittleEndian, &port)
 
-	n1, err1 := conn.Read(rawheader)
-	n2, err2 := conn.Read(rawIP)
-	n3, err3 := conn.Read(rawPort)
-	fmt.Println("Read:", n1, err1, n2, err2, n3, err3)
-	fmt.Printf("rawheader: %+v, rawip %+v, rawPort: %+v", rawheader, rawIP, rawPort)
-	_, _ = binary.Uvarint(rawheader)
-	ipInt := binary.LittleEndian.Uint32(rawIP)
-	ip := make(net.IP, 4)
-	binary.BigEndian.PutUint32(ip, ipInt)
-	port := binary.LittleEndian.Uint16(rawPort)
-	return &net.TCPAddr{
-		IP:   ip,
-		Port: int(port),
+	if instr != 6 {
+		return nil, fmt.Errorf("Unimplemented instruction: %d", instr)
 	}
+
+	return &net.TCPAddr{
+		IP:   []byte{ip[3], ip[2], ip[1], ip[0]},
+		Port: int(port),
+	}, nil
 }
--- src/code.cloudfoundry.org/tcpbinder/tcpbinder_test.go
+++ src/code.cloudfoundry.org/tcpbinder/tcpbinder_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -17,8 +18,7 @@ import (
 
 var _ = Describe("Binder test", func() {
 	var session *gexec.Session
-
-	BeforeEach(func() {
+	BeforeSuite(func() {
 		bin, err := gexec.Build("code.cloudfoundry.org/tcpbinder")
 		Expect(err).NotTo(HaveOccurred())
 		session, err = gexec.Start(exec.Command("sudo", "--non-interactive", bin), GinkgoWriter, GinkgoWriter)
@@ -26,28 +26,55 @@ var _ = Describe("Binder test", func() {
 		Eventually(session).Should(gbytes.Say("Listening on socket at /var/tmp/com.docker.vmnetd.socket"))
 	})
 
-	AfterEach(func() {
+	AfterSuite(func() {
 		gexec.Start(exec.Command("sudo", "--non-interactive", "kill", fmt.Sprintf("%d", session.Command.Process.Pid)), GinkgoWriter, GinkgoWriter)
 		gexec.KillAndWait()
 		gexec.CleanupBuildArtifacts()
 	})
 
-	It("binds ports", func() {
-		conn, err := net.DialUnix("unix", nil, &net.UnixAddr{
+	var conn *net.UnixConn
+	BeforeEach(func() {
+		var err error
+		conn, err = net.DialUnix("unix", nil, &net.UnixAddr{
 			Net:  "unix",
 			Name: "/var/tmp/com.docker.vmnetd.socket",
 		})
 		Expect(err).NotTo(HaveOccurred())
+	})
+	AfterEach(func() {
+		conn.Close()
+	})
 
+	It("binds ports", func() {
 		Expect(sendHello(conn, "VMN3T", 22, "0123456789012345678901234567890123456789")).To(Succeed())
 		Expect(recvHello(conn)).To(Equal("CFD3V"))
 		Expect(sendBindAddr(conn, "10.245.0.2", 1888)).To(Succeed())
-		ln, err := recvBindAddr(conn, "10.245.0.2", 1888)
+		ln, _, err := recvBindAddr(conn, "10.245.0.2", 1888)
 		Expect(err).NotTo(HaveOccurred())
+		defer ln.Close()
 
 		msg := "Hello from test"
 		go sendMessage("10.245.0.2", 1888, msg)
 		Expect(readFromListener(ln)).To(Equal(msg))
+	})
+
+	Context("binding to a bound port", func() {
+		var prior net.Listener
+		BeforeEach(func() {
+			var err error
+			prior, err = net.Listen("tcp", "10.245.0.2:1888")
+			Expect(err).NotTo(HaveOccurred())
+		})
+		AfterEach(func() { prior.Close() })
+
+		It("sends an error", func() {
+			Expect(sendHello(conn, "VMN3T", 22, "0123456789012345678901234567890123456789")).To(Succeed())
+			Expect(recvHello(conn)).To(Equal("CFD3V"))
+			Expect(sendBindAddr(conn, "10.245.0.2", 1888)).To(Succeed())
+			_, b, _ := recvBindAddr(conn, "10.245.0.2", 1888)
+			fmt.Println("msg received", b)
+			Expect(b[0]).To(Equal(uint8(48)))
+		})
 	})
 })
 
@@ -79,28 +106,34 @@ func sendBindAddr(conn *net.UnixConn, ip string, port uint16) error {
 	return binary.Write(conn, binary.LittleEndian, port)
 }
 
-func recvBindAddr(conn *net.UnixConn, ip string, port uint16) (ln net.Listener, err error) {
+func recvBindAddr(conn *net.UnixConn, ip string, port uint16) (net.Listener, []byte, error) {
 	b := make([]byte, 8, 8)
 	oob := make([]byte, 16, 16)
 	if _, _, _, _, err := conn.ReadMsgUnix(b, oob); err != nil {
-		return nil, err
+		return nil, b, err
+	}
+	if b[0] != 0 {
+		return nil, b, errors.New("Look at b")
 	}
 	scms, err := syscall.ParseSocketControlMessage(oob)
 	if err != nil {
-		return nil, err
+		return nil, b, err
 	}
 	fds, err := syscall.ParseUnixRights(&scms[0])
 	if err != nil {
-		return nil, err
+		return nil, b, err
 	}
 	file := os.NewFile(uintptr(fds[0]), fmt.Sprintf("tcp:%s:%d"))
-	return net.FileListener(file)
+	defer file.Close()
+	ln, err := net.FileListener(file)
+	return ln, b, err
 }
 
 func sendMessage(host string, port uint16, mesg string) {
 	defer GinkgoRecover()
 	wconn, err := net.Dial("tcp", "10.245.0.2:1888")
 	Expect(err).NotTo(HaveOccurred())
+	defer wconn.Close()
 	wconn.Write([]byte(mesg))
 }
 
@@ -109,6 +142,7 @@ func readFromListener(ln net.Listener) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	defer conn.Close()
 	received := make([]byte, 15, 15)
 	_, err = conn.Read(received)
 	if err != nil {
