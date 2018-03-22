12c821ba66f3fc643b158c9bae7f5fe2da011e76
Dave Goddard <dave@goddard.id.au>
2018-03-22 14:24:54 -0400

    chmod
    Signed-off-by: Steve Hiehn <shiehn@pivotal.io>

--- src/code.cloudfoundry.org/cfdev/run-tests.sh
+++ src/code.cloudfoundry.org/cfdev/run-tests.sh
@@ -11,6 +11,7 @@ extend_sudo_timeout() {
 
 disable_sudo() {
     if [ ! -z "${NONPRIV_USER:-}" ] ; then
+        chmod go+w $GOPATH
         sudo -E su $NONPRIV_USER -c "$*"
     else
         sudo -E -k "$@"
