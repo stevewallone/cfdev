4f62dc46462654a9608a9fd0f13dcb0537bf5117
Dave Goddard <dave@goddard.id.au>
2018-05-31 07:34:47 -0400

    wip
    Signed-off-by: Andy Voellmer <avoellmer@pivotal.io>

--- src/code.cloudfoundry.org/cfdev/cmd/start/start.go
+++ src/code.cloudfoundry.org/cfdev/cmd/start/start.go
@@ -184,9 +184,15 @@ func (s *Start) RunE(_ *cobra.Command, _ []string) error {
 	}
 
 	s.UI.Say("Deploying CF...")
-	go reportCfDeployProgress(s.UI, garden)
+	go reportDeployProgress(s.UI, garden, "cf")
 	if err := gdn.DeployCloudFoundry(garden, registries); err != nil {
 		return errors.SafeWrap(err, "Failed to deploy the Cloud Foundry")
+	}
+
+	s.UI.Say("Deploying Mysql...")
+	go reportDeployProgress(s.UI, garden, "cf-mysql")
+	if err := gdn.DeployMysql(garden); err != nil {
+		return errors.SafeWrap(err, "Failed to deploy mysql")
 	}
 
 	s.UI.Say(`
@@ -283,12 +289,12 @@ func (s *Start) watchLaunchd(label string) {
 	}()
 }
 
-func reportCfDeployProgress(UI UI, garden client.Client) {
+func reportDeployProgress(UI UI, garden client.Client, deploymentName string) {
 	ui := singlelinewriter.New(UI.Writer())
 	ui.Say("  Uploading Releases")
 	b, err := bosh.New(garden)
 	if err == nil {
-		ch := b.VMProgress()
+		ch := b.VMProgress(deploymentName)
 		for p := range ch {
 			if p.Total > 0 {
 				ui.Say("  Progress: %d of %d (%s)", p.Done, p.Total, p.Duration.Round(time.Second))
