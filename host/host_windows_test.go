2ea8f464b226e3f88ce01a3e5cf42265fa31cd10
Anthony Emengo <aemengo@pivotal.io>
2018-09-10 11:50:00 -0400

    wip

--- host/host_windows_test.go
+++ host/host_windows_test.go
@@ -5,38 +5,67 @@ import (
 
 	"code.cloudfoundry.org/cfdev/errors"
 	"code.cloudfoundry.org/cfdev/host"
+	"code.cloudfoundry.org/cfdev/hosts/mocks"
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Host", func() {
+
+	var (
+		mockController *gomock.Controller
+		mockPowershell *mocks.MockPowershell
+		h			   *host.Host
+	)
+
+	BeforeEach(func() {
+		mockController = gomock.NewController(GinkgoT())
+		mockPowershell = mocks.NewPowershell(mockController)
+		h = &host.Host{
+			Powershell: mockPowershell,
+		}
+	})
+
 	Describe("check requirements", func() {
+		Context("when not running in an admin shell", func() {
+			It("returns an error", func() {
+				mockPowershell.EXPECT(`IsInRole`).Return("FALSE", nil)
+
+				err := h.CheckRequirements()
+				Expect(err.Error()).To(ContainSubstring(`Running without admin privileges: You must run cf dev with an admin privileged powershell`))
+				Expect(errors.SafeError(err)).To(Equal("Running without admin privileges"))
+			})
+		})
+
 		Context("when running in an admin shell", func() {
-			//we always run windows tests in an admin shell
-			Context("when hyperv is enabled", func() {
-				// we assume tests always run on a machine with hyperv enabled
+			Context("Hyper-V is enabled on a Windows 10 machine", func() {
 				It("succeeds", func() {
-					h := &host.Host{}
+					mockPowershell.EXPECT(`IsInRole`).Return("TRUE", nil)
+					mockPowershell.EXPECT(`(Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State`).Return("Enabled", nil)
+					mockPowershell.EXPECT(gomock.Any).AnyTimes()
+
 					Expect(h.CheckRequirements()).To(Succeed())
 				})
 			})
-			Context("when hyperv is disabled", func() {
-				BeforeEach(func() {
-					cmd := exec.Command("powershell.exe", "-Command",
-						"Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart")
-					output, err := cmd.CombinedOutput()
-					Expect(err).NotTo(HaveOccurred(), string(output))
-				})
 
-				AfterEach(func() {
-					cmd := exec.Command("powershell.exe", "-Command",
-						"Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart")
-					output, err := cmd.CombinedOutput()
-					Expect(err).NotTo(HaveOccurred(), string(output))
-				})
+			Context("Hyper-V is enabled on a Windows Server 2016 Machine", func() {
+				It("succeeds", func() {
+					mockPowershell.EXPECT(`IsInRole`).Return("TRUE", nil)
+					mockPowershell.EXPECT(`(Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Management-PowerShell -Online).State`).Return("Enabled", nil)
+					mockPowershell.EXPECT(gomock.Any).AnyTimes()
 
-				It("fails", func() {
-					h := &host.Host{}
+					Expect(h.CheckRequirements()).To(Succeed())
+				})
+			})
+
+			Context("Hyper-V is disabled", func() {
+				It("returns an error", func() {
+					mockPowershell.EXPECT(`IsInRole`).Return("TRUE", nil)
+					mockPowershell.EXPECT(`(Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State`).Return("Disabled", nil)
+					mockPowershell.EXPECT(`(Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Management-PowerShell -Online).State`).Return("Disabled", nil)
+					mockPowershell.EXPECT(gomock.Any).AnyTimes()
+
 					err := h.CheckRequirements()
 					Expect(err.Error()).To(ContainSubstring(`Hyper-V disabled: You must first enable Hyper-V on your machine`))
 					Expect(errors.SafeError(err)).To(Equal("Hyper-V disabled"))
